8ee83e0d0ce85528ac68a84e3b7790c075f1dab7
raoulelysee <raoulelysee@gmail.com>
2015-01-22 21:40:58 -0500

    Ajouter un System out Print

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -17,6 +17,7 @@ public class HelloWorld {
     public static void main(String[] args) {
        
         System.out.println("Hello world");
+        System.out.println("Raoul Elysee");
     }
     
 }
